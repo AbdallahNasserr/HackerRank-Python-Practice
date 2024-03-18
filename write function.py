47e7178d16a32c251c8e75d45271f58e1e4f21ad
AbdallahNasserr <137197588+AbdallahNasserr@users.noreply.github.com>
2024-03-18 17:42:35 +0200

    Update write function.py

--- write function.py
+++ write function.py
@@ -2,13 +2,16 @@ def is_leap(year):
     leap = False
     
     # Write your logic here
-    if year%4==0:
-        leap = True
+    if year % 4 == 0:  # Check if the year is divisible by 4
+        if year % 100 == 0:  # Check if the year is divisible by 100
+            if year % 400 == 0:  # Check if the year is divisible by 400
+                leap = True
+            else:
+                leap = False
+        else:
+            leap= True
     else:
-        if year%100!=0:
-            leap = False
-        elif year % 400 == 0:
-            leap = True
+        leap = False
     return leap
 
 year = int(input())
